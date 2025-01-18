3bdaaf75eb548c8d8c50e4ce35cee873bec1a610
Veronika Palto <nikalalin@gmail.com>
2025-01-18 14:56:20 +0300

    Add jest to the project

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@state/(.*)$': '<rootDir>/src/state/$1',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
+
